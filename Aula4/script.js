6933704ae675da6f0c23f7875719600a7cfeadec
hik <henrikmezedes@gmail.com>
2025-09-11 17:06:27 -0300

    aula 5/JavaScript

---
+++ Aula4/script.js
@@ -0,0 +1,22 @@
+a = 1;
+b = 2;
+if(b < a){
+
+}
+else if(b > a){
+
+}
+else{
+
+}
+i = 0;
+while(i <= 10){
+    console.log(i);
+    i++
+}
+
+for(let i = 0; i < 10; i++){
+    console.log("Exemplo, for" + i)
+
+}
+    
