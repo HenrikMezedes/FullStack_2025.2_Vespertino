fd0391c84e4fe221d1c716dda656ff027244ffe4
hik <henrikmezedes@gmail.com>
2025-09-18 16:41:10 -0300

    aula6

---
+++ aula6/script.js
@@ -0,0 +1,26 @@
+//  let titulo = document.getElementById.apply('titulo').innerHTML;
+// console.log('titulo')
+
+// document.getElementById
+// let nome = window.prompt("Qual o seu nome?");
+// let idade = window.prompt("Qual o sua idade?");
+// let ano = window.prompt("Qual o ano atual?");
+
+// document.getElementById("resposta_ex1").innerHTML;
+
+// let resposta ="Ola" + nome + "sua idade é" + idade + "seu ano de nascimento é " + ano;
+
+// function exibeMenssagem(texto){
+//     console.log {texto}
+
+//     exibeMenssagem()
+//     exibeMenssagem()
+//     exibeMenssagem()
+//     exibeMenssagemComParametro(texto 1)  
+//     exibeMenssagemComParametro(texto 2)
+//     exibeMenssagemComParametro(texto 3)
+
+    
+
+// }
+
